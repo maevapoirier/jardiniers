76a15eb63301e7fe325fef23a2b4d644e8d6567f
maeva.poirier2022@campus-eni.fr <Axel15042020>
2022-10-20 16:59:03 +0200

    recherche des zones

--- src/Repository/LocalizationRepository.php
+++ src/Repository/LocalizationRepository.php
@@ -2,9 +2,13 @@
 
 namespace App\Repository;
 
+use App\Entity\City;
 use App\Entity\Localization;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\ResultSetMapping;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use Doctrine\Persistence\ManagerRegistry;
+use DoctrineExtensions\Query\Mysql\Radians;
 
 /**
  * @extends ServiceEntityRepository<Localization>
@@ -39,28 +43,50 @@ class LocalizationRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Localization[] Returns an array of Localization objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('l.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * @return Localization[] Returns an array of Localization objects
+     */
+    public function findGardeners(City $value): array
+    {
 
-//    public function findOneBySomeField($value): ?Localization
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        $lat = $value->getLatitude();
+        $lon = $value->getLongitude();
+
+        $queryBuilder = $this->createQueryBuilder('l')
+                                ->join('l.city', 'c')
+                                ->andWhere('((2 * ATAN2(SQRT(POWER(SIN(RADIANS(c.latitude - :lat) / 2.0), 2) + COS(RADIANS(:lat)) * COS(RADIANS(c.latitude)) * POWER(SIN(RADIANS(c.longitude - :lon) / 2.0), 2)), SQRT(1 - POWER(SIN(RADIANS(c.latitude - :lat) / 2.0), 2) + COS(RADIANS(:lat)) * COS(RADIANS(c.latitude)) * POWER(SIN(RADIANS(c.longitude - :lon) / 2.0), 2)))) * :r /1000) <= l.radius')
+                                ->setParameter('r', 6371000)
+                                ->setParameter('lat', $lat)
+                                ->setParameter('lon', $lon);
+        $query = $queryBuilder->getQuery();
+        
+        return  $query->getResult();
+
+
+        
+    }
+
+
+
+    // REQUETE SQL SELECT *, ((2 * atan2(sqrt(POW(sin(radians(c.latitude - 43.935441391) / 2.0), 2) + cos(radians(43.935441391)) * cos(radians(c.latitude)) * POW(sin(radians(c.longitude - 4.841098248) / 2.0), 2)), sqrt(1 - POW(sin(radians(c.latitude - 43.935441391) / 2.0), 2) + cos(radians(43.935441391)) * cos(radians(c.latitude)) * POW(sin(radians(c.longitude - 4.841098248) / 2.0), 2)))) * 6371000 /1000) FROM localization l INNER JOIN city c ON l.city_id = c.id
+
+
+    public function afficherCoordonnees(City $value)
+    {
+
+        $lat = $value->getLatitude();
+        $lon = $value->getLongitude();
+
+        dd($lat, $lon);
+    }
+
+    //    public function findOneBySomeField($value): ?Localization
+    //    {
+    //        return $this->createQueryBuilder('l')
+    //            ->andWhere('l.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
